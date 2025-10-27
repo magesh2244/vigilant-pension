d703cc082f07a58854d62e224eb7ec7721f9850d
Magesh2424 <magesh2408@gmail.com>
2025-10-27 09:55:08 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "https://github.com/magesh2244/vigilant-pension/blob/main/dist/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
